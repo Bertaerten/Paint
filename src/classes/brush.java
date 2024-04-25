86bad4ef860ab8c1def0e7e43727b019ac083988
Bert <Bertil.bjerring@gmail.com>
2024-04-25 08:24:53 +0200

    fixed at man kan tegne uden for kanterne

--- src/classes/brush.java
+++ src/classes/brush.java
@@ -8,6 +8,7 @@ public class brush {
     
 PApplet p;
 int r;
+int minX=50, maksX=250, minY=50, maksY=250;
 
 public brush(int radius, PApplet s){
 r=radius;
@@ -22,7 +23,12 @@ public void setPixels(){
 
     for(int i=0; i<r ; i++){
         for(int j=r;j>0;j--){
-            p.set(p.mouseX+(i-(r/2)), p.mouseY-(j-(r/2)), p.color(0));
+            if(p.mouseX+(i-(r/2)) > minX && p.mouseX+(i-(r/2))< maksX){
+                if (p.mouseY-(j-(r/2)) > minY && p.mouseY-(j-(r/2)) < maksY) {
+                    p.set(p.mouseX+(i-(r/2)), p.mouseY-(j-(r/2)), p.color(0));
+                }
+                
+            }
         }
     }
 
