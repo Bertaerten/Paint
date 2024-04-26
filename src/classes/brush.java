7674d0fe16df1353dc9346a5c7e80f4babf76dd8
Bert <Bertil.bjerring@gmail.com>
2024-04-26 09:57:01 +0200

    start på fade

--- src/classes/brush.java
+++ src/classes/brush.java
@@ -1,5 +1,6 @@
 package classes;
 import java.applet.Applet;
+//import math.*;
 
 import processing.core.*;
 
@@ -10,11 +11,13 @@ PApplet p;
 int r;
 int minX=50, maksX=250, minY=50, maksY=250;
 int Color;
+float haardhed;
 
 public brush(int radius, int ColorIN , PApplet s){
 r=radius;
  p=s;
 Color = ColorIN;
+haardhed=0.5f;
 /*
  if (r%2 != 0){ // størrelsen af brushen skal være et lige tal
     r++;
@@ -24,10 +27,18 @@ Color = ColorIN;
 public void setPixels(){
 
     for(int i=0; i<r ; i++){
-        for(int j=r;j>0;j--){
+        for(int j=0;j<r;j++){
             if(p.mouseX+(i-(r/2)) > minX && p.mouseX+(i-(r/2))< maksX){
                 if (p.mouseY-(j-(r/2)) > minY && p.mouseY-(j-(r/2)) < maksY) {
-                    p.set(p.mouseX+(i-(r/2)), p.mouseY-(j-(r/2)), Color);
+                    double pw1= (r/2)-i;
+                    double pw2= (r/2)-j;
+
+                    double l = Math.sqrt( Math.pow(pw1,2) + Math.pow(pw2, 2) );// (i-x)^2 + (j-y)^2
+                    System.out.println(l);
+                    int alpha = (int) (r/l); 
+                    int farve = p.color(0,0,0, alpha);
+
+                    p.set(p.mouseX+(i-(r/2)), p.mouseY-(j-(r/2)), farve);
                 }
                 
             }
@@ -43,9 +54,6 @@ public void ChangeSize(int change){
 public void setColor(int newColor){
     Color=newColor;
 }
-
-    public boolean Is
-
 }
 
 
