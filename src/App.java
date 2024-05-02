54ca269fbd5e20ba1979cea411278d2ad91a5abb
Simoin Lund <you@example.com>
2024-05-02 14:12:55 +0200

    MethodDone

--- src/App.java
+++ src/App.java
@@ -50,10 +50,21 @@ public class App  extends PApplet{
 
     int kind;
 
+    brush bill;
+    brush hvid1;
+    button bros;
+    
+
+    int brain = color(100, 150, 100);
+    int bruh = color(10, 10, 100);
+
+
     public void settings(){
         size(1920, 1080);
 
         indsæt = new udseende(this);
+
+        
 
 
         farve1Stor = new button(this, sort, 50, 1050, 50);
@@ -94,8 +105,8 @@ public class App  extends PApplet{
     
         updatePixels();
 
+        colorClicked();
 
-        
         
 
 
@@ -128,6 +139,112 @@ public class App  extends PApplet{
         if(mouseY<1080 && mouseY>200)
         set(mouseX, mouseY, color(0));
     }   
-    
+ }
+
+public void colorClicked(){
+    if (farve1Stor.isClicked()){
+        kind = 1;
+      }
+      else if (farve2Stor.isClicked()){
+        kind = 2;
+      }
+
+  if (kind == 1){
+      if(sortCirkel.isClicked()){
+          farve1Stor.setColor(sortCirkel.getColor());
+      }
+      if(hvidCirkel.isClicked()){
+          farve1Stor.setColor(hvidCirkel.getColor());
+      }
+      if(sandCirkel.isClicked()){
+          farve1Stor.setColor(sandCirkel.getColor());
+      }
+      if(grønCirkel.isClicked()){
+          farve1Stor.setColor(grønCirkel.getColor());
+      }
+      if(blåCirkel.isClicked()){
+          farve1Stor.setColor(blåCirkel.getColor());
+      }
+      if(gråCirkel.isClicked()){
+          farve1Stor.setColor(gråCirkel.getColor());
+      }
+      if(pinkCirkel.isClicked()){
+          farve1Stor.setColor(pinkCirkel.getColor());
+      }
+      if(lillaCirkel.isClicked()){
+          farve1Stor.setColor(lillaCirkel.getColor());
+      }
+      if(hvidGulCirkel.isClicked()){
+          farve1Stor.setColor(hvidGulCirkel.getColor());
+      }
+      if(blåSortCirkel.isClicked()){
+          farve1Stor.setColor(blåSortCirkel.getColor());
+      }
+      if(lillaV2Cirkel.isClicked()){
+          farve1Stor.setColor(lillaV2Cirkel.getColor());
+      }
+      if(måskeRødCirkel.isClicked()){
+          farve1Stor.setColor(måskeRødCirkel.getColor());
+      }
+      if(hotPinkCirkel.isClicked()){
+          farve1Stor.setColor(hotPinkCirkel.getColor());
+      }
+      if(gulCirkel.isClicked()){
+          farve1Stor.setColor(gulCirkel.getColor());
+      }
+      if(grøn2Cirkel.isClicked()){
+          farve1Stor.setColor(grøn2Cirkel.getColor());
+      }
   }
+
+
+  if (kind == 2){
+      if(sortCirkel.isClicked()){
+          farve2Stor.setColor(sortCirkel.getColor());
+      }
+      if(hvidCirkel.isClicked()){
+          farve2Stor.setColor(hvidCirkel.getColor());
+      }
+      if(sandCirkel.isClicked()){
+          farve2Stor.setColor(sandCirkel.getColor());
+      }
+      if(grønCirkel.isClicked()){
+          farve2Stor.setColor(grønCirkel.getColor());
+      }
+      if(blåCirkel.isClicked()){
+          farve2Stor.setColor(blåCirkel.getColor());
+      }
+      if(gråCirkel.isClicked()){
+          farve2Stor.setColor(gråCirkel.getColor());
+      }
+      if(pinkCirkel.isClicked()){
+          farve2Stor.setColor(pinkCirkel.getColor());
+      }
+      if(lillaCirkel.isClicked()){
+          farve2Stor.setColor(lillaCirkel.getColor());
+      }
+      if(hvidGulCirkel.isClicked()){
+          farve2Stor.setColor(hvidGulCirkel.getColor());
+      }
+      if(blåSortCirkel.isClicked()){
+          farve2Stor.setColor(blåSortCirkel.getColor());
+      }
+      if(lillaV2Cirkel.isClicked()){
+          farve2Stor.setColor(lillaV2Cirkel.getColor());
+      }
+      if(måskeRødCirkel.isClicked()){
+          farve2Stor.setColor(måskeRødCirkel.getColor());
+      }
+      if(hotPinkCirkel.isClicked()){
+          farve2Stor.setColor(hotPinkCirkel.getColor());
+      }
+      if(gulCirkel.isClicked()){
+          farve2Stor.setColor(gulCirkel.getColor());
+      }
+      if(grøn2Cirkel.isClicked()){
+          farve2Stor.setColor(grøn2Cirkel.getColor());
+      }
+  }
+}
+
 }
